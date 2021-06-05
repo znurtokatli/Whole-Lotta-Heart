eac6c8a3346871189c7eb20ad8cf028c3138bb18
LJeanParrish <ljeanparrish@gmail.com>
2021-06-05 11:34:47 -0400

    updated app.py file

--- app.py
+++ app.py
@@ -34,7 +34,23 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.html")
- 
+
+@app.route('/chart1')
+def chart1():
+    return render_template("java.html")   #java
+
+@app.route('/chart2')
+def chart2():
+    return render_template("ml.html")   #machine learning data
+
+@app.route('/chart3')
+def chart3():
+    return render_template("tableau.html")   #tableau charts
+
+# @app.route('/api/data')
+# def api():
+#     return TBD
+
 
 if __name__ == '__main__':
     app.run(debug = True)
